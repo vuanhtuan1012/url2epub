1b8e6f33e1ae4748f2d6f6f1a2dd3001db503795
VU Anh Tuan <vuanhtuan1012@gmail.com>
2021-02-06 21:50:28 +0100

    remove unused file

--- constants.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Author: anh-tuan.vu
-# @Date:   2021-02-04 21:40:20
-# @Last Modified by:   anh-tuan.vu
-# @Last Modified time: 2021-02-04 22:20:47
-
-# kept tags: <b>, <strong>, <em>, <p>, <i>, <img>, <sup>
-HTML_KEPT_TAGS = ['b', 'strong', 'em', 'p', 'i', 'sup']
